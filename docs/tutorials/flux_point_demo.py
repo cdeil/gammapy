5216cd1615e99620c6c847032a54e9db3bff0b34
ellisowen <ellis.owen@mpi-hd.mpg.de>
2014-07-03 16:37:22 +0200

    update

--- docs/tutorials/flux_point_demo.py
+++ docs/tutorials/flux_point_demo.py
@@ -2,7 +2,8 @@
 """
 import numpy as np
 from astropy.table import Table
-from gammapy.spectrum.flux_point import compute_differential_flux_points
+from gammapy.spectrum.flux_point import compute_differential_flux_points, _energy_lafferty_power_law
+from gammapy.spectrum.powerlaw import power_law_eval, power_law_integral_flux
 import matplotlib.pyplot as plt
 
 SPECTRAL_INDEX = 4
@@ -23,21 +24,21 @@ def my_spectrum(x):
                     1e-5 * (x / E_3) ** g3, 1e-9 * (x / E_4) ** g4,
                     1e-14 * (x / E_5) ** g5, 0])
     return im
+  
     
-    
-def get_flux_tables(table, y_method, function):
+def get_flux_tables(table, y_method, function, spectral_index):
     table1 = table.copy()
     lafferty_flux = compute_differential_flux_points(table1, 'lafferty', y_method,
-                                                     function, SPECTRAL_INDEX)
+                                                     function, spectral_index)
     table2 = table.copy()
     log_flux = compute_differential_flux_points(table2, 'log_center', y_method,
-                                                function, SPECTRAL_INDEX)
+                                                function, spectral_index)
     return lafferty_flux, log_flux
 
 
 def plot_flux_points(table, x, y, function, energy_min, energy_max, y_method):
     f, axarr = plt.subplots(2, sharex=True)
-    lafferty_flux, log_flux = get_flux_tables(table, y_method, function)
+    lafferty_flux, log_flux = get_flux_tables(table, y_method, function, SPECTRAL_INDEX)
 
     axarr[0].loglog(x, (x ** 2) * y)
     axarr[0].loglog(lafferty_flux['ENERGY'],
@@ -91,3 +92,106 @@ def plot_plaw():
                            energy_min, energy_max, 'power_law')
     plt.legend()
     return plt
+
+
+def make_gamma_plot():
+    energy_min = np.array([3])
+    energy_max = np.array([1000])
+    energies = np.array(_energy_lafferty_power_law(energy_min, energy_max, SPECTRAL_INDEX))
+    diff_flux =  power_law_eval(energies, 1, SPECTRAL_INDEX, 1)
+    int_flux = power_law_integral_flux(diff_flux, SPECTRAL_INDEX, energies, energy_min, energy_max)
+    # Put data into table
+    table = Table()
+    table['ENERGY_MIN'] = energy_min
+    table['ENERGY_MAX'] = energy_max
+    table['INT_FLUX'] = int_flux
+    
+    spectral_indices = np.arange(1.1, 6, 0.01)
+    for spectral_index in spectral_indices:
+        lafferty_flux, log_flux = get_flux_tables(table, 'power_law', None, spectral_index)
+        residuals_lafferty = ((lafferty_flux['DIFF_FLUX']
+                              - diff_flux) / diff_flux) * 100
+        plt.plot(spectral_index, residuals_lafferty, marker='D',
+                     color='k', linewidth=0, ms=1)
+        #residuals_log = ((log_flux['DIFF_FLUX']
+        #                     - diff_flux) / diff_flux) * 100
+        #plt.plot(spectral_index, residuals_log, marker='D',
+        #             color='r', linewidth=0, ms=1)
+    
+   #indices = np.arange(len(energy_min))
+   # for index in indices:
+   #     plt.vlines(energy_min[index], -100, 800, color='k', linestyle=':')
+    #plt.hlines(0, 0, 10, color='k')
+    plt.ylabel('Residuals/%')
+    plt.xlabel('Spectral Index')
+    #plt.xlim([1, 10000])
+    plt.show()
+    
+def make_x_plot():
+    energy_min = np.array([3])
+    energy_max = np.array([1000])
+    energies = np.array(_energy_lafferty_power_law(energy_min, energy_max, SPECTRAL_INDEX))
+    diff_flux =  power_law_eval(energies, 1, SPECTRAL_INDEX, 1)
+    int_flux = power_law_integral_flux(diff_flux, SPECTRAL_INDEX, energies, energy_min, energy_max)
+    # Put data into table
+    table = Table()
+    table['ENERGY_MIN'] = energy_min
+    table['ENERGY_MAX'] = energy_max
+    table['INT_FLUX'] = int_flux
+    
+    spectral_indices = np.arange(1.1, 6, 0.01)
+    for spectral_index in spectral_indices:
+        lafferty_flux, log_flux = get_flux_tables(table, 'power_law', None, spectral_index)
+        residuals_lafferty = ((lafferty_flux['ENERGY']
+                              - energy_min) / (energy_max-energy_min))
+        plt.plot(spectral_index, residuals_lafferty, marker='D',
+                     color='k', linewidth=0, ms=1)
+        residuals_log = ((log_flux['ENERGY']
+                              - energy_min) / (energy_max-energy_min))
+        plt.plot(spectral_index, residuals_log, marker='D',
+                     color='r', linewidth=0, ms=1)
+    
+   #indices = np.arange(len(energy_min))
+   # for index in indices:
+   #     plt.vlines(energy_min[index], -100, 800, color='k', linestyle=':')
+    #plt.hlines(0, 0, 10, color='k')
+    plt.ylabel('X position in bin')
+    plt.xlabel('Spectral Index')
+    #plt.xlim([1, 10000])
+    plt.show()
+    
+def make_dex_plot():
+    # TODO (in progress)
+    energy_min = np.array([3])
+    energy_max = np.array([1000])
+    energies = np.array(_energy_lafferty_power_law(energy_min, energy_max, SPECTRAL_INDEX))
+    diff_flux =  power_law_eval(energies, 1, SPECTRAL_INDEX, 1)
+    int_flux = power_law_integral_flux(diff_flux, SPECTRAL_INDEX, energies, energy_min, energy_max)
+    # Put data into table
+    table = Table()
+    table['ENERGY_MIN'] = energy_min
+    table['ENERGY_MAX'] = energy_max
+    table['INT_FLUX'] = int_flux
+    
+
+    lafferty_flux, log_flux = get_flux_tables(table, 'power_law', None, spectral_index)
+    residuals_lafferty = ((lafferty_flux['DIFF_FLUX']
+                            - diff_flux) / diff_flux) * 100
+    plt.plot(spectral_index, residuals_lafferty, marker='D',
+                    color='k', linewidth=0, ms=1)
+        #residuals_log = ((log_flux['DIFF_FLUX']
+        #                     - diff_flux) / diff_flux) * 100
+        #plt.plot(spectral_index, residuals_log, marker='D',
+        #             color='r', linewidth=0, ms=1)
+    
+   #indices = np.arange(len(energy_min))
+   # for index in indices:
+   #     plt.vlines(energy_min[index], -100, 800, color='k', linestyle=':')
+    #plt.hlines(0, 0, 10, color='k')
+    plt.ylabel('Residuals/%')
+    plt.xlabel('Spectral Index')
+    #plt.xlim([1, 10000])
+    plt.show()
+    
+if __name__ == '__main__':
+    make_x_plot()
