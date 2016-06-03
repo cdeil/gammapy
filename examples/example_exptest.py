9b44fee25d380dd541e2beee004ada8aa8df26d8
Christoph Deil <Deil.Christoph@gmail.com>
2016-06-03 10:51:32 +0200

    Add time exptest

---
+++ examples/example_exptest.py
@@ -0,0 +1,7 @@
+"""Example how to run the exptest variability test.
+"""
+import gammapy
+print(gammapy)
+
+from gammapy.time import exptest
+exptest.run_exptest()
---
+++ gammapy/time/exptest.py
@@ -0,0 +1,12 @@
+# Licensed under a 3-clause BSD style license - see LICENSE.rst
+from __future__ import absolute_import, division, print_function, unicode_literals
+
+__all__ = [
+    'run_exptest',
+]
+
+
+
+
+def run_exptest():
+    print('aaa')
